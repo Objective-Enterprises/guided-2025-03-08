85105470a20a716d949966ecf32d5a5b7ac8f68f
David Y. Stephenson <david@davidystephenson.com>
2025-03-08 12:55:34 -0500

    Conditional page rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import LoginPage from "./pages/LoginPage";
+import ProductsListPage from "./pages/ProductsListPage";
 
 const App = () => {
+  const localUser = localStorage.getItem('user')
+  const user = JSON.parse(localUser)
+  const page = user ? <ProductsListPage /> : <LoginPage />
   return (
     <>
       <Header />
-      <LoginPage />
+      {page}
       <Footer />
     </>
   );
