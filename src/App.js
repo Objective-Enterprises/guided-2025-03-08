72b76251539c7709fe97849b79d6c19799fb44d2
David Y. Stephenson <david@davidystephenson.com>
2025-03-08 12:26:18 -0500

    Basic login structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import LoginPage from "./pages/LoginPage";
+
 const App = () => {
   return (
     <>
       <Header />
+      <LoginPage />
+      <Footer />
     </>
   );
 };
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import { Button, Container, Form } from 'react-bootstrap'
+
+export default function LoginPage () {
+  return (
+    <Container>
+      <Form>
+        <Button type='submit'>Submit</Button>
+      </Form>
+    </Container>
+  )
+}
